c576ebe186109f87f6b02f8bc66576037db0e68c
M Bentar Raya T <bentarraya01@gmail.com>
2025-10-23 15:46:08 +0000

    posisikan ditengah (_for element p_)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 export function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-foreground md:text-left">
+      <div className="container flex h-24 items-center justify-center">
+        <p className="text-balance text-center text-sm leading-loose text-foreground">
           © {new Date().getFullYear()} Muhammad Bentar Raya Triadmodjo. All
           Rights Reserved.
         </p>
