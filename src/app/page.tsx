695cc2788032ef9882df6d1176024cf024d05d46
M Bentar Raya T <bentarraya01@gmail.com>
2025-10-23 05:00:52 +0000

    Kalo begitu bersihkan semua kode baris yang mengatur background website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import { AnimateOnScroll } from '@/components/motion/AnimateOnScroll';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow overflow-x-hidden">
         <AnimateOnScroll>
