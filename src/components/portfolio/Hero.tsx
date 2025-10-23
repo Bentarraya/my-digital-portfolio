c2b9705a7aee0707cab94307d1fd66971884b829
M Bentar Raya T <bentarraya01@gmail.com>
2025-10-23 04:56:31 +0000

    Tapi masih muncul background yang berwana #2F284E dan background yang da

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
   const profilePicture = PlaceHolderImages.find(p => p.id === 'profile-picture');
 
   return (
-    <div className="w-full py-20 sm:py-24 lg:py-32 bg-background">
+    <div className="w-full py-20 sm:py-24 lg:py-32">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center text-center">
           <div className="mb-8">
