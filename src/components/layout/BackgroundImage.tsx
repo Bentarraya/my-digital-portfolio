16004f774ff9f23740e03e531ebf481e5082c220
M Bentar Raya T <bentarraya01@gmail.com>
2025-10-24 11:26:15 +0000

    terlihat ada bug pada tampilan background terlihat bagian putih ketika d

--- src/components/layout/BackgroundImage.tsx
+++ src/components/layout/BackgroundImage.tsx
@@ -5,7 +5,6 @@ export function BackgroundImage() {
       className="fixed inset-0 z-0 bg-cover bg-center"
       style={{
         backgroundImage: "url('/images/background.png')",
-        backgroundColor: "hsl(var(--background-end-hsl))",
       }}
     />
   );
